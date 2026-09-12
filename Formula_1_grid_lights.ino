bd2c9e8c6d7152924fef63257bf7332512b46a1f
jasonmungure-code <jasonmungure7@gmail.com>
2026-09-12 06:12:17 +0200

    Update Formula_1_grid_lights.ino

--- Formula_1_grid_lights.ino
+++ Formula_1_grid_lights.ino
@@ -4,37 +4,48 @@ void setup() {
   pinMode(10,OUTPUT);
   pinMode(11,OUTPUT);
   pinMode(12,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(2, INPUT);
 
 }
 
 void loop() {
-  digitalWrite(12, HIGH);
-  digitalWrite(11, LOW);
-  digitalWrite(10, LOW);
-  digitalWrite(8, LOW);
-  delay(1000);
+  int buttonstate = digitalRead(2);
 
-  digitalWrite(12, HIGH);
-  digitalWrite(11, HIGH);
-  digitalWrite(10, LOW);
-  digitalWrite(8, LOW);
-  delay(1000);
+  if(buttonstate == HIGH) {
 
-  digitalWrite(12, HIGH);
-  digitalWrite(11, HIGH);
-  digitalWrite(10, HIGH);
-  digitalWrite(8, LOW);
-  delay(1000);
+    digitalWrite(12, HIGH);
+    digitalWrite(11, LOW);
+    digitalWrite(10, LOW);
+    digitalWrite(8, LOW);
+    tone(6, 1000, 200);
+    delay(1000);
 
-  digitalWrite(12, HIGH);
-  digitalWrite(11, HIGH);
-  digitalWrite(10, HIGH);
-  digitalWrite(8, HIGH);
-  delay(random(1000, 3000));
+    digitalWrite(12, HIGH);
+    digitalWrite(11, HIGH);
+    digitalWrite(10, LOW);
+    digitalWrite(8, LOW);
+    tone(6, 1000, 200);
+    delay(1000);
 
-  digitalWrite(12, LOW);
-  digitalWrite(11, LOW);
-  digitalWrite(10, LOW);
-  digitalWrite(8, LOW);
-  delay(5000);
+    digitalWrite(12, HIGH);
+    digitalWrite(11, HIGH);
+    digitalWrite(10, HIGH);
+    digitalWrite(8, LOW);
+    tone(6, 1000, 200);
+    delay(1000);
+
+    digitalWrite(12, HIGH);
+    digitalWrite(11, HIGH);
+    digitalWrite(10, HIGH);
+    digitalWrite(8, HIGH);
+    tone(6, 1000, 200);
+    delay(random(1000, 2000));
+
+    digitalWrite(12, LOW);
+    digitalWrite(11, LOW);
+    digitalWrite(10, LOW);
+    digitalWrite(8, LOW);
+    delay(5000);
+  }
 }
